64b15181dcfd77bc1032f5990d92dc462ee09894
diogojapiassu <diogojapiassu@gmail.com>
2014-05-08 16:41:47 -0300

    Adicionado DataOutputStream

--- Integracao/src/Requisicao.java
+++ Integracao/src/Requisicao.java
@@ -1,40 +1,51 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.Authenticator.RequestorType;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
-
 
 public class Requisicao {
 
-	public static void main(String[] args) throws MalformedURLException, IOException { 
+	public static void main(String[] args) {
 		// monta estrutura de parametros a serem eviados
-					String data = URLEncoder.encode( "nome", "UTF-8" ) + "=" + URLEncoder.encode( "Fernando", "UTF-8" );
-		 
-				         URL url = new URL( "http://www.google.com" );
-			                 HttpURLConnection httpUrlConnection = (HttpURLConnection)url.openConnection();
-		 
-			                 // envia dados
-			                 httpUrlConnection.setRequestMethod("POST");
-			                 httpUrlConnection.setDoOutput(true);
-			                 OutputStreamWriter outputWriter = new OutputStreamWriter(httpUrlConnection.getOutputStream());
-			                 outputWriter.write(data);
-			                 outputWriter.flush();
-		 
-			                 // Obtem as respostas
-			                 InputStreamReader inputReader = new InputStreamReader(httpUrlConnection.getInputStream());
-			                 BufferedReader bufferedReader = new BufferedReader( inputReader );
-		 
-					System.out.println( "\n** retorno da p·gina web **" );
-					String linha = "";
-					while ( (linha = bufferedReader.readLine()) != null){
-						System.out.println(linha);
-					}
+		String data;
+		try {
+			data = URLEncoder.encode("nome", "UTF-8") + "=" + URLEncoder.encode("Fernando", "UTF-8");
+
+			// URL url = new URL("http://www.google.com");
+			URL url = new URL("https://www.googleapis.com/upload/storage/v1/b/myBucket/o?uploadType=media&name=myObject");
+			HttpURLConnection httpUrlConnection = (HttpURLConnection) url.openConnection();
+
+			// envia dados
+			httpUrlConnection.setRequestMethod("POST");
+			httpUrlConnection.setRequestProperty("Host", "www.googleapis.com");
+			httpUrlConnection.setRequestProperty("Content-Type", "image/jpeg");
+			// httpUrlConnection.setRequestProperty("Host",
+			// "www.googleapis.com");
+			httpUrlConnection.setDoOutput(true);
+
+			// OutputStreamWriter outputWriter = new
+			// OutputStreamWriter(httpUrlConnection.getOutputStream());
+			DataOutputStream outputWriter = new DataOutputStream(httpUrlConnection.getOutputStream());
+
+			outputWriter.write(data.getBytes());
+			outputWriter.flush();
+
+			// Obtem as respostas
+			InputStreamReader inputReader = new InputStreamReader(httpUrlConnection.getInputStream());
+			BufferedReader bufferedReader = new BufferedReader(inputReader);
+
+			System.out.println("\n** retorno da p√°gina web **");
+			String linha = "";
+			while ((linha = bufferedReader.readLine()) != null) {
+				System.out.println(linha);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
