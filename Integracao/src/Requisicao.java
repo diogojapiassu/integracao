026cd1963bdb9e5caf1ef50e6a122966ad6fddcb
diogojapiassu <diogojapiassu@gmail.com>
2014-06-15 15:52:33 -0300

    Método para inserir imagens no Cloud Storage

--- Integracao/src/Requisicao.java
+++ Integracao/src/Requisicao.java
@@ -1,7 +1,8 @@
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,7 +10,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 
 import org.json.simpleForBukkit.JSONObject;
 import org.json.simpleForBukkit.parser.JSONParser;
@@ -18,18 +18,87 @@ import org.json.simpleForBukkit.parser.ParseException;
 public class Requisicao {
 
 	public static void main(String[] args) {
+		String nomeDaImagem = "ferrari.png";
+		String chaveAutenticacao = "Bearer ya29.LgAvA6skHcGpGCIAAADYAlXO8tk5DdXKZw4zdRIdXkoWleuc3rz2uEn6rmXyTNWzPqwHJ0G64XKW9fTZtNU";
+		String nomeDoArquivoParaGravacao = "imagemSalvaFerrari.png";
+		
+		recuperarImagem(nomeDaImagem, chaveAutenticacao, nomeDoArquivoParaGravacao);
+		
+		String nomeArquivoParaInsercao = "ferrari.png";
+		String chaveAutenticacaoParaGravacao = "Bearer ya29.LgAvA6skHcGpGCIAAADYAlXO8tk5DdXKZw4zdRIdXkoWleuc3rz2uEn6rmXyTNWzPqwHJ0G64XKW9fTZtNU";
+		
+		//gravarImagem(nomeArquivoParaInsercao, chaveAutenticacaoParaGravacao);
+	}
+
+	private static void gravarImagem(String nomeArquivoParaInsercao, String chaveAutenticacaoParaGravacao) {
+		// monta estrutura de parametros a serem eviados
+				//String data;
+				try {
+					//data = URLEncoder.encode("nome", "UTF-8") + "=" + URLEncoder.encode("Fernando", "UTF-8");
+
+					URL url = new URL("https://www.googleapis.com/upload/storage/v1/b/ufg/o/?uploadType=media&predefinedAcl=publicRead&name=" + nomeArquivoParaInsercao);
+					//URL url = new URL("https://www.googleapis.com/upload/storage/v1/b/myBucket/o?uploadType=media&name=myObject");
+					HttpURLConnection httpUrlConnection = (HttpURLConnection) url.openConnection();
+
+					// envia dados
+					httpUrlConnection.setRequestMethod("POST");
+					httpUrlConnection.setRequestProperty("Authorization", chaveAutenticacaoParaGravacao);
+					httpUrlConnection.setRequestProperty("Content-Type", "image/png");
+					// httpUrlConnection.setRequestProperty("Host",
+					// "www.googleapis.com");
+					httpUrlConnection.setDoOutput(true);
+
+					//JSONObject jo = new JSONObject();
+					//JSONObject data = new JSONObject();
+					
+					InputStream in;
+					
+					in = new FileInputStream(nomeArquivoParaInsercao);
+					
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+					for ( int i; (i = in.read()) != -1; ) {
+					    out.write(i);
+					}
+					
+					byte[] data = out.toByteArray();
+					
+					in.close();
+					out.close();
+					
+					//OutputStreamWriter outputWriter = new OutputStreamWriter(httpUrlConnection.getOutputStream());
+					DataOutputStream outputWriter = new DataOutputStream(httpUrlConnection.getOutputStream());
+
+					outputWriter.write(data);
+					outputWriter.flush();
+					outputWriter.close();
+
+					// Obtem as respostas
+					InputStreamReader inputReader = new InputStreamReader(httpUrlConnection.getInputStream());
+					BufferedReader bufferedReader = new BufferedReader(inputReader);
+					
+					 String retornoDaPagina = getRetornoPagina(bufferedReader);
+					 System.out.println(retornoDaPagina);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		
+	}
+
+	private static void recuperarImagem(String nomeDoArquivo, String chaveAutenticacao, String nomeDoArquivoParaGravacao) {
 		// monta estrutura de parametros a serem eviados
 		String data;
 		try {
-			data = URLEncoder.encode("nome", "UTF-8") + "=" + URLEncoder.encode("Fernando", "UTF-8");
+			//data = URLEncoder.encode("nome", "UTF-8") + "=" + URLEncoder.encode("Fernando", "UTF-8");
 
-			URL url = new URL("https://www.googleapis.com/storage/v1/b/ufg/o/ufg.png");
+			URL url = new URL("https://www.googleapis.com/storage/v1/b/ufg/o/" + nomeDoArquivo);
 			//URL url = new URL("https://www.googleapis.com/upload/storage/v1/b/myBucket/o?uploadType=media&name=myObject");
 			HttpURLConnection httpUrlConnection = (HttpURLConnection) url.openConnection();
 
 			// envia dados
 			httpUrlConnection.setRequestMethod("GET");
-			httpUrlConnection.setRequestProperty("Authorization", "Bearer ya29.KgCzGk6Ohv7IlSIAAAAFM3kz6LUF4YMWMkRrmEns3R_YIi64Ko0RiaXYy1DGz_2xmNRCuGlBUj2iAnmwPXs");
+			httpUrlConnection.setRequestProperty("Authorization", chaveAutenticacao);
 			//httpUrlConnection.setRequestProperty("Content-Type", "image/jpeg");
 			// httpUrlConnection.setRequestProperty("Host",
 			// "www.googleapis.com");
@@ -47,22 +116,22 @@ public class Requisicao {
 			InputStreamReader inputReader = new InputStreamReader(httpUrlConnection.getInputStream());
 			BufferedReader bufferedReader = new BufferedReader(inputReader);
 			
-			imprimeRetornoPagina(bufferedReader);
+			String retorno = getRetornoPagina(bufferedReader);
+			converteTextoEmJson(retorno, nomeDoArquivoParaGravacao);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 	
-	private static void salvarImagemPorURL(String urlDaImagem){
+	private static void salvarImagemPorURL(String urlDaImagem, String nomeDoArquivoParaGravacao){
 		try {
 			URL url = new URL(urlDaImagem);
 			InputStream in;
 		
 			in = new BufferedInputStream(url.openStream());
 			
-			OutputStream out = new FileOutputStream("imagemSalva.png");
+			OutputStream out = new FileOutputStream(nomeDoArquivoParaGravacao);
 
 			for ( int i; (i = in.read()) != -1; ) {
 			    out.write(i);
@@ -76,9 +145,8 @@ public class Requisicao {
 		
 	}
 
-	private static void imprimeRetornoPagina(BufferedReader bufferedReader)
+	private static String getRetornoPagina(BufferedReader bufferedReader)
 			throws IOException {
-		System.out.println("\n** retorno da página web **");
 		String linha = "";
 		String texto = "";
 		while ((linha = bufferedReader.readLine()) != null) {
@@ -86,20 +154,18 @@ public class Requisicao {
 			texto += linha + "\n";
 		}
 		
+		return texto;
+	}
+
+	private static void converteTextoEmJson(String texto, String nomeDoArquivoParaGravacao) {
 		JSONParser jp = new JSONParser();
 		try {
 			JSONObject jo = (JSONObject) jp.parse(texto);
 			
 			 String caminho = jo.get("mediaLink").toString();
 			
-			 salvarImagemPorURL(caminho);
-			 
-//			File file = new File(caminho);
-//			
-//			FileOutputStream fos = new FileOutputStream(file);
-			
+			 salvarImagemPorURL(caminho, nomeDoArquivoParaGravacao);
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
